251a699c17d9e62a728a83c917f7034fdf6cd8e7
Ian VanSchooten <ian.vanschooten@gmail.com>
2021-11-09 14:06:21 -0500

    Add missing cms config file

---
+++ src/scripts/cms/netlify-cms-config.ts
@@ -0,0 +1,101 @@
+import type {CmsConfig} from "netlify-cms-core";
+import {ALL_LOCALES} from '../../languages';
+
+export const config: CmsConfig = {
+  load_config_file: false,
+  media_folder: "public/images/uploads",
+  public_folder: "/images/uploads",
+  // Must be running netlify-cms-proxy-server for local editing to work correctly
+  local_backend: true,
+  backend: {
+    name: "github",
+    branch: "main",
+    repo: "DefinedNet/nebula-docs",
+    squash_merges: true,
+  },
+  collections: [
+    {
+      name: "pages",
+      label: "Pages",
+      label_singular: "Page",
+      delete: false,
+      create: true,
+      folder: "src/data/docs",
+      i18n: true,
+      slug: "{{fields.slug}}",
+      fields: [
+        { name: "title", label: "Title", widget: "string", i18n: true },
+        { name: "slug", label: "Slug", widget: "string", i18n: true },
+        { name: "summary", label: "Summary", widget: "text", i18n: true },
+        { name: "body", label: "Body", widget: "markdown", i18n: true },
+      ]
+    },
+    {
+      name: "settings",
+      label: "Settings",
+      delete: false,
+      editor: { preview: false },
+      files: [
+        {
+          name: "locales",
+          label: "Locales",
+          file: "src/data/settings/locales.json",
+          description: "Currently supported language locales.  If adding another, also edit i18n.locales in scripts/cms/config.ts",
+          fields: [
+            {
+              name: "locales",
+              label: "Locales",
+              label_singular: "Locale",
+              widget: "list",
+              fields: [
+                { 
+                  name: "language", 
+                  label: "Language", 
+                  widget: "string", 
+                  hint: "Native name of the language, endonym (e.g. Español)",
+                },
+                {
+                  name: "locale",
+                  label: "Locale code",
+                  widget: "string",
+                  hint: "Two character locale code (ISO 639-1)",
+                  pattern: ["^([a-z]){2}$", "Must be two lowercase letters."],
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "sidebar",
+          label: "Sidebar config",
+          file: "src/data/settings/sidebar.json",
+          description: "Organization of pages in sidebar",
+          fields: [
+            {
+              name: "sidebar",
+              label: "Docs pages",
+              label_singular: "Docs page",
+              widget: "list",
+              fields: [
+                {
+                  name: "page",
+                  label: "Page",
+                  widget: "relation",
+                  collection: "pages",
+                  search_fields: ["title"],
+                  value_field: "{{slug}}",
+                  display_fields: ["title"],
+                },
+              ],
+            },
+          ],
+        }
+      ],
+    }
+  ],
+  i18n: {
+    structure: "multiple_folders",
+    default_locale: "en",
+    locales: ALL_LOCALES,
+  }
+}
