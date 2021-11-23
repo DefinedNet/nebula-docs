8cc5ae7032b716bbf0d0ac13cbd2ef9e99191300
Ian VanSchooten <ian.vanschooten@gmail.com>
2021-11-23 10:51:42 -0500

    Adjust mobile nav styles

--- src/get-page-links.ts
+++ src/get-page-links.ts
@@ -1,26 +1,40 @@
 import sidebarConfig from './data/settings/sidebar.json';
 import {getLanguageFromURL, getSlugFromFilename, getLanguageFromFilename, DEFAULT_LOCALE} from './languages';
 
-
 export function getSections({currentPage, allPages}) {
   const langCode = getLanguageFromURL(currentPage);
 
-  function buildPageObj(slug: string) {
-    // Find the right file and the right language
-    let page = allPages.find(p => 
-      getSlugFromFilename(p.file.pathname) === slug && getLanguageFromFilename(p.file.pathname) === langCode
-    );
-    // If there wasn't a translated version, fall back to default.
-    if (!page) {
-      page = allPages.find(p => getSlugFromFilename(p.file.pathname) === slug && getLanguageFromFilename(p.file.pathname) === DEFAULT_LOCALE);
-    }
-
-    return {title: page.title, href: `/${langCode}/${page.slug}` }
-  }
-
   return sidebarConfig.sidebar.map(section => {
-    const root = buildPageObj(section.rootPage);
-    const children = section.pages?.map(p => buildPageObj(p.page));
+    const root = buildPageObj(section.rootPage, langCode, allPages);
+    const children = section.pages?.map(p => buildPageObj(p.page, langCode, allPages));
     return {root, children};
   });
+}
+
+export function getPageLinks({currentPage, allPages}) {
+  const langCode = getLanguageFromURL(currentPage);
+
+  return sidebarConfig.sidebar.map(section => {
+    const root = buildPageObj(section.rootPage, langCode, allPages);
+    const children = section.pages?.map(p => buildPageObj(p.page, langCode, allPages)) ?? [];
+    
+    // Don't want to include root page if there are children
+    if (children.length) {
+      return children
+    }
+    return [root];
+  }).flat();
+}
+
+function buildPageObj(slug: string, langCode: string, allPages: any) {
+  // Find the right file and the right language
+  let page = allPages.find(p => 
+    getSlugFromFilename(p.file.pathname) === slug && getLanguageFromFilename(p.file.pathname) === langCode
+  );
+  // If there wasn't a translated version, fall back to default.
+  if (!page) {
+    page = allPages.find(p => getSlugFromFilename(p.file.pathname) === slug && getLanguageFromFilename(p.file.pathname) === DEFAULT_LOCALE);
+  }
+
+  return {title: page.title, href: `/${langCode}/${page.slug}` }
 }
