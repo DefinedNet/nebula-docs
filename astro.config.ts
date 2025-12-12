3688584e51b84505d1b31cb5fabbba6a89ba8117
Caleb Jasik <calebjasik@jasik.xyz>
2025-12-12 17:54:26 -0600

    Add site url for sitemap support

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nebula.defined.net',
   integrations: [
     starlight({
       title: 'Nebula',
