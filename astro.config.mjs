eb62accb8eea9e4ba4c12464f22b3bf115b4d995
Caleb Jasik <calebjasik@jasik.xyz>
2025-12-12 16:40:31 -0600

    Rename astro.config.ts

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import react from '@astrojs/react';
-// https://astro.build/config
-export default defineConfig({
-  integrations: [starlight(), react()]
-});
---
+++ astro.config.ts
@@ -0,0 +1,8 @@
+import react from '@astrojs/react';
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight(), react()],
+});
